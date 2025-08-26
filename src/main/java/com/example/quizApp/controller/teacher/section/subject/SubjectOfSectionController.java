561c63b70c9b794fdd00bf714c5c0baf760aa37f
rowel <razonabler23@gmail.com>
2025-08-26 11:43:16 +0800

    fix persisting subject

--- src/main/java/com/example/quizApp/controller/teacher/section/subject/SubjectOfSectionController.java
+++ src/main/java/com/example/quizApp/controller/teacher/section/subject/SubjectOfSectionController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.quizApp.dto.subject.AddSubjectRequest;
-import com.example.quizApp.result.teacher.subject.SubjectResult;
 import com.example.quizApp.service.teacher.sectionHandle.subjectHandle.SubjectService;
 
 import jakarta.validation.Valid;
@@ -22,12 +21,13 @@ public class SubjectOfSectionController {
 	private final SubjectService subjectService;
 	
 	@PostMapping("section/{sectionName}/addsubject")
-	ResponseEntity<SubjectResult.Add> addSubject(@PathVariable String sectionName, @RequestBody @Valid AddSubjectRequest req) {
+	ResponseEntity<String> addSubject(@PathVariable String sectionName, @RequestBody @Valid AddSubjectRequest req) {
 		
 		var result = subjectService.addSubject(sectionName, req);
+		var subjectName = req.getSubjectName();
 		
 		return switch (result) {
-		case SUBJECT_ALREADY_EXISTS -> ResponseEntity.status(409).build();
+		case SUBJECT_ALREADY_EXISTS -> ResponseEntity.status(409).body("subject: " + subjectName + " already exists");
 		case SUBJECT_ADDED_SUCCESS -> ResponseEntity.status(204).build();
 		};
 	}
--- src/main/java/com/example/quizApp/model/teacher/sectionHandle/subjectHandle/Subject.java
+++ src/main/java/com/example/quizApp/model/teacher/sectionHandle/subjectHandle/Subject.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,7 +29,7 @@ public class Subject {
 	
 	@Setter @Getter
 	@ManyToOne
-	@JoinColumn(name = "section_owner")
+	@JoinColumn(name = "section_owner", referencedColumnName = "name")
 	private Section sectionOwner;
 	
 	Subject() {}
